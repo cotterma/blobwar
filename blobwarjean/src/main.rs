fe9113f2ee45a73fae235faafd10fec53fbdd3ac
Jean Conan <jean.conan@grenoble-inp.org>
2023-02-28 18:56:50 +0100

    mesure du temps

--- blobwarjean/src/main.rs
+++ blobwarjean/src/main.rs
@@ -2,10 +2,16 @@ extern crate blobwar;
 //use blobwar::board::Board;
 use blobwar::configuration::Configuration;
 use blobwar::strategy::{MinMax};
+use std::time::Instant;
+
+
 
 fn main() {
     //let board = Board::load("x").expect("failed loading board");
     let board = Default::default();
     let mut game = Configuration::new(&board);
+    let start = Instant::now();
     game.battle(MinMax(3), MinMax(2));
+    let duration = start.elapsed();
+    println!("Time elapsed is: {:?}", duration);
 }
--- blobwarmatteo/src/main.rs
+++ blobwarmatteo/src/main.rs
@@ -2,10 +2,14 @@ extern crate blobwar;
 //use blobwar::board::Board;
 use blobwar::configuration::Configuration;
 use blobwar::strategy::{Greedy, Human, MinMax};
+use std::time::Instant;
 
 fn main() {
     //let board = Board::load("x").expect("failed loading board");
     let board = Default::default();
     let mut game = Configuration::new(&board);
-    game.battle(MinMax(5), MinMax(2));
+    let start = Instant::now();
+    game.battle(MinMax(3), MinMax(2));
+    let duration = start.elapsed();
+    println!("Time elapsed is: {:?}", duration);
 }
