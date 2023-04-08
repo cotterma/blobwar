abb173bdebea63c8cdb323f0495d3a79718325ea
Jean Conan <jean.conan@grenoble-inp.org>
2023-04-08 20:04:23 +0200

    parallèle

--- blobwarjean/src/strategy/alphabeta.rs
+++ blobwarjean/src/strategy/alphabeta.rs
@@ -3,6 +3,9 @@ use std::fmt;
 use super::Strategy;
 use crate::configuration::{Configuration, Movement};
 use crate::shmem::AtomicMove;
+use rayon::iter::ParallelBridge;
+use rayon::iter::ParallelIterator;
+
 
 /// Anytime alpha beta algorithm.
 /// Any time algorithms will compute until a deadline is hit and the process is killed.
@@ -28,7 +31,7 @@ impl fmt::Display for AlphaBeta {
 impl Strategy for AlphaBeta {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
         return state.movements().max_by_key(|movement: &Movement| 
-            nega_alpha_beta(self.0-1, &state.play(movement), -127, 127));
+            nega_alpha_beta_para(self.0-1, &state.play(movement), -127, 127));
     }
 }
 
@@ -44,6 +47,34 @@ fn nega_alpha_beta(depth: u8, state : &Configuration, mut alpha: i8, beta: i8) -
         let mut value;
         for movement in state.movements() {
             value = nega_alpha_beta(depth - 1, &state.play(&movement), -beta, -alpha);
+            if best_value < value {
+                best_value = value;
+            }
+            if best_value >= beta {
+                return -best_value;
+            }
+            if alpha < best_value {
+                alpha = best_value;
+            }
+        }
+        return -best_value;// si on gagne de 1, notre adversaire gagne de -1 aka il perd de 1
+    }
+}
+fn nega_alpha_beta_para(depth: u8, state : &Configuration, mut alpha: i8, beta: i8) -> i8 {
+    if depth == 0{
+        return state.value();
+    }
+    else if depth == 1 {
+        return -state.movements().par_bridge().map(|movement| state.play(&movement).value()).max().unwrap();
+    }
+    else if state.movements().peekable().peek().is_none(){
+        return -nega_alpha_beta(depth-1, &state.skip_play(), -beta, -alpha);
+    }
+    else{
+        let mut best_value = -127;
+        let mut value;
+        for movement in state.movements() {
+            value = nega_alpha_beta_para(depth - 1, &state.play(&movement), -beta, -alpha);
             if best_value < value {
                 best_value = value;
             }
